cb1b7ef5ef3701dcb96f67f78509f6a9f6f715bc
lalitha <lalithapelluri6@gmail.com>
2019-03-07 14:42:18 -0800

    some changes to js file

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+
 $("button").on("click", function() {
     var Animals = $(this).attr("data-animal");
     var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + Animals + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -12,8 +13,10 @@ $("button").on("click", function() {
                 gifDiv.addClass('gif-images');
 
                 for (var i = 0; i < results.length; i++) {
-                    var animalImage = $("<img id='img_gif'>");
-
+                    var animalImage = $("<img class='img_gif'>");
+                      animalImage.attr("data-state", 'still');
+                      animalImage.attr("data-still", results[i].images.fixed_width_still.url);
+                      animalImage.attr("data-animate", results[i].images.fixed_width.url);
                     var rating = results[i].rating;
 
                     var p = $("<p>").text("Rating: " + rating);
@@ -27,4 +30,15 @@ $("button").on("click", function() {
 
     });
 
+});
+
+$(document).on("click", '.img_gif', function() {
+   if($(this).attr('data-state') === 'still'){
+       $(this).attr('data-state', 'animate');
+       $(this).attr('src', $(this).attr("data-animate"))
+       console.log('hey hellooo')
+   } else {
+       $(this).attr('data-state', 'still');
+       $(this).attr('src', $(this).attr("data-still"))
+   }
 });
